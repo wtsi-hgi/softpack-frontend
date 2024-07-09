cfef8c7038a8a2c676ea8917d333cd580744d140
Michael Woolnough <mw31@sanger.ac.uk>
2024-07-09 14:46:49 +0100

    Add module -> YML parser.

---
+++ module/module.go
@@ -0,0 +1,92 @@
+package module
+
+import (
+	_ "embed"
+	"io"
+	"strconv"
+	"strings"
+	"text/template"
+)
+
+var (
+	//go:embed readme.tmpl
+	readme         string
+	readmeTemplate = template.Must(template.New("").Parse(readme))
+)
+
+const whitespace = " \t\n"
+
+func ToSoftpackYML(name string, contents string) io.Reader {
+	var (
+		inHelp               bool
+		packages             = []string{""}
+		description, version string
+	)
+
+	for _, line := range strings.Split(contents, "\n") {
+		line = strings.Trim(line, whitespace)
+
+		if inHelp {
+			if line == "}" {
+				inHelp = false
+			} else if strings.HasPrefix(line, "puts stderr") {
+				line, _ = strconv.Unquote(strings.ReplaceAll(strings.Trim(strings.TrimPrefix(line, "puts stderr"), whitespace), "\\$", "$"))
+
+				description += "  " + line + "\n"
+			}
+		} else if strings.HasPrefix(line, "proc ModulesHelp") {
+			inHelp = true
+		} else if strings.HasPrefix(line, "module-whatis ") {
+			line, _ = strconv.Unquote(strings.Trim(strings.TrimPrefix(line, "module-whatis"), whitespace))
+			line = strings.Trim(line, whitespace)
+
+			if strings.HasPrefix(line, "Name:") {
+				if line = strings.TrimPrefix(line, "Name:"); line != "" {
+					parts := strings.Split(line, ":")
+					nameParts := strings.FieldsFunc(parts[0], func(r rune) bool {
+						switch r {
+						case ' ', '\n', '\t':
+							return true
+						}
+
+						return false
+					})
+
+					if nameParts[0] != "" {
+						name = nameParts[0]
+					}
+
+					if len(parts) > 1 {
+						version = strings.Trim(parts[1], whitespace)
+					}
+				}
+			} else if strings.HasPrefix(line, "Version:") {
+				vers := strings.Fields(strings.TrimPrefix(line, "Version:"))
+				if len(vers) > 0 && vers[0] != "" {
+					version = vers[0]
+				}
+			} else if strings.HasPrefix(line, "Packages:") {
+				for _, pkg := range strings.FieldsFunc(strings.Trim(strings.TrimPrefix(line, "Packages:"), whitespace), func(r rune) bool {
+					switch r {
+					case ' ', '\t', ',', '\n':
+						return true
+					}
+
+					return false
+				}) {
+					if pkg != "" {
+						packages = append(packages, pkg)
+					}
+				}
+			}
+		}
+	}
+
+	if version != "" {
+		name += "@" + version
+	}
+
+	packages[0] = name
+
+	return strings.NewReader("description: |\n" + description + "packages:\n  - " + strings.Join(packages, "\n  - ") + "\n")
+}
---
+++ module/module_test.go
@@ -0,0 +1,34 @@
+package module
+
+import (
+	"bytes"
+	"embed"
+	"io"
+	"path/filepath"
+	"strings"
+	"testing"
+)
+
+//go:embed files/*
+var files embed.FS
+
+func TestToSoftPack(t *testing.T) {
+	entries, _ := files.ReadDir("files")
+
+	var n int
+
+	for _, entry := range entries {
+		if name := entry.Name(); strings.HasSuffix(entry.Name(), ".mod") {
+			n++
+
+			tName := strings.TrimSuffix(name, ".mod")
+
+			contents, _ := files.ReadFile(filepath.Join("files", name))
+			expected, _ := files.ReadFile(filepath.Join("files", tName+".yml"))
+
+			if yml, _ := io.ReadAll(ToSoftpackYML(tName, string(contents))); !bytes.Equal(yml, expected) {
+				t.Errorf("test %d: expecting contents %q, got %q", n, expected, yml)
+			}
+		}
+	}
+}
